53a6dc36a2a4af196ef91bc1a5ddbfcbe8d987cb
Roj10 <renanjussiani@gmail.com>
2024-03-21 18:16:09 -0300

    ajuste js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function aviso(){
     alert("Quer café :) ?");
 }
@@ -16,25 +15,5 @@ function nextimage(){
     if(count>5){
         count = 1;
     }
-
-    document.getElementById("radio"+count).checked = true;
-function aviso(){
-    alert("Quer café :) ?");
 }
-
-let count = 1;
-
-document.getElementById("radio1").checked = true;
-
-setInterval(function(){
-    nextimage();
-},5000)
-
-function nextimage(){
-    count++;
-    if(count>5){
-        count = 1;
-    }
-
     document.getElementById("radio"+count).checked = true;
-}
